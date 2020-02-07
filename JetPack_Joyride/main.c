49dc0896de1b822556f7699107e51f4a2f9398fc
PavoJ <xxnanni2002xx@gmail.com>
2020-02-07 22:26:31 +0100

    +Aggiunto la funzione menu, per aggiungere menu con estrema facilità +Aggiunto un prototipo di movimento

--- JetPack_Joyride/main.c
+++ JetPack_Joyride/main.c
@@ -1,42 +1,51 @@
-#include<SFML\Graphics.h>
-#include<SFML\window.h>
-#include<stdio.h>
+#include"logica.h"
+#include"gameClock.h"
 
-void mainMenu()
+#include <SFML\Graphics.h>
+#include <SFML\window.h>
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int menu(sfRenderWindow* win, sfVideoMode vMode, sfColor clearColor, int argc, ...)
 {
-	//dichiaro la finestra
-	sfRenderWindow* win;
+	int clickedButton = -1;
+	int activeButton = -1;
 
-	//imposto la dimensione della finestra
-	sfVideoMode vMode;
-	vMode.width = 800;
-	vMode.height = 600;
-
-	//creazione della finestra
-	win = sfRenderWindow_create(vMode, "Menu' Principale", sfClose, NULL);
+	va_list list;
+	va_start(list, argc);
 	
-	sfFont *defaultFont = sfFont_createFromFile("defaultFont.ttf");
+	sfFont* defaultFont = sfFont_createFromFile("defaultFont.ttf");
 
-	sfText* options[3];
-	const char *optionsT[] = { "Gioca", "Impostazioni", "Esci" };
+	sfText** options = malloc(argc*sizeof(sfText*));
 
+	sfFloatRect rect;
 	int a;
-	for (a = 0; a < 3; a++)
+	for (a = 0; a < argc; a++)
 	{
 		options[a] = sfText_create();
 
-		sfText_setString(options[a], optionsT[a]);
+		sfText_setString(options[a], va_arg(list, char*));
 		sfText_setColor(options[a], sfBlack);
-		sfText_setPosition(options[a], (sfVector2f) { 40, 100*(a+1) });
+
 		sfText_setFont(options[a], defaultFont);
 		sfText_setCharacterSize(options[a], 70);
+
+		rect = sfText_getLocalBounds(options[a]);
+
+		sfText_setOrigin(options[a], (sfVector2f) { rect.width / 2, rect.height / 2 });
+
+		sfText_setPosition(options[a], (sfVector2f) { (vMode.width / 2), (vMode.height / (argc + 1)) * (a + 1) });
 	}
 
 	sfFloatRect playR;
 
 	sfVector2i mouse;
 
-	while (sfRenderWindow_isOpen(win))
+	//finchè il giocatore non ha ancora scelto un tasto
+	while (clickedButton==-1)
 	{
 		sfEvent event;
 		while (sfRenderWindow_pollEvent(win, &event))
@@ -48,42 +57,60 @@ void mainMenu()
 				break;
 			
 			case sfEvtMouseMoved:
-				for (a = 0; a < 3; a++)
+				for (a = 0; a < argc; a++)
 				{
 					mouse = sfMouse_getPositionRenderWindow(win);
 					playR = sfText_getGlobalBounds(options[a]);
 
 					if (sfFloatRect_contains(&playR, (float)mouse.x, (float)mouse.y))
-						sfText_setColor(options[a], sfColor_fromRGB(150, 150, 150));
+					{
+						sfText_setColor(options[a], sfColor_fromRGB(70, 70, 150));
+						activeButton = a;
+					}
 					else
+					{
 						sfText_setColor(options[a], sfBlack);
+						if (activeButton == a)
+						{
+							activeButton = -1;
+						}
+					}
+				}
+				break;
+
+			case sfEvtMouseButtonPressed:
+				if (sfMouse_isButtonPressed(sfMouseLeft))
+				{
+					if (activeButton != -1)
+					{
+						clickedButton = activeButton;
+					}
 				}
 				break;
 			}
 		}
 
-		sfRenderWindow_clear(win, sfWhite);
+		sfRenderWindow_clear(win, clearColor);
 
-		for(a=0 ; a<3 ; a++)
+		for(a=0 ; a<argc ; a++)
 			sfRenderWindow_drawText(win, options[a], NULL);
 
 		sfRenderWindow_display(win);
 	}
+
+	return clickedButton;
 }
 
-void gameLoop()
+void gameLoop(sfRenderWindow* win, sfVideoMode vMode)
 {
-	//dichiaro la finestra
-	sfRenderWindow* win;
+	sfRectangleShape* mainChar = sfRectangleShape_create();//Player
+	sfRectangleShape_setSize(mainChar, (sfVector2f) { 50, 100 });
+	sfRectangleShape_setFillColor(mainChar, sfBlue);
 
-	//imposto la dimensione della finestra
-	sfVideoMode vMode;
-	vMode.width = 1920;
-	vMode.height = 1080;
+	bool movingUp = false;//se il tasto per andare su è tenuto premuto.
 
-	//creazione della finestra
-	win = sfRenderWindow_create(vMode, "hey", sfClose, NULL);
-
+	player* pl = getPS();
+	
 	//finchè la finestra è aperta
 	while (sfRenderWindow_isOpen(win))
 	{
@@ -96,21 +123,55 @@ void gameLoop()
 			case sfEvtClosed://chiusura finestra
 				sfRenderWindow_close(win);
 				break;
+
+			case sfEvtKeyPressed:
+				if (sfKeyboard_isKeyPressed(sfKeySpace))
+					movingUp = true;
+				break;
+
+			case sfEvtKeyReleased:
+				if (!sfKeyboard_isKeyPressed(sfKeySpace))
+					movingUp = false;
+				break;
 			}
+		}
+
+		if (elapsedTick())
+		{
+			move(movingUp);
+			sfRectangleShape_setPosition(mainChar, (sfVector2f) { pl->position.x, 1080 - pl->position.y });
+			/*
+			system("cls");
+			printf("%d - %d (%d)", pl->position.x, 1080-pl->position.y, movingUp);*/
 		}
 
 		//sovrascrivo tutta la finestra con il colore bianco
 		sfRenderWindow_clear(win, sfColor_fromRGB(255, 255, 255));
 
+		sfRenderWindow_drawRectangleShape(win, mainChar, NULL);
 
 		//disegno i cambiamenti su schermo
 		sfRenderWindow_display(win);
 	}
 }
 
-int main()
+int main ()
 {
-	mainMenu();
+	//dichiarazione della finestra
+	sfRenderWindow* win;
 
-	
+	//imposto la dimensione della finestra
+	sfVideoMode vMode;
+	vMode.width = 1920;
+	vMode.height = 1080;
+
+	//creazione della finestra
+	win = sfRenderWindow_create(vMode, "Jetpack vs. Zombies", sfClose, NULL);
+
+	int inp = menu(win, vMode, sfColor_fromRGB(150, 150, 150), 3, "Gioca", "Impostazioni", "Esci");
+	switch (inp)
+	{
+	case 0:
+		gameLoop(win, vMode);
+	}
 }
